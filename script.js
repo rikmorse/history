99ffaffa3892085210fe21c3ca052d76c72882da
R Morse <rikmorse@gmail.com>
2018-05-15 15:52:48 -0600

    New History Web Site
    more practice

---
+++ script.js
@@ -0,0 +1,10 @@
+// global variables
+// var age = getElementsByName("ageRange").value;
+
+function range(el) {
+	if (el.value){ 
+		alert ("Your age is " + el.value);
+	} else {
+		alert ("please select your age!");
+	}
+}
